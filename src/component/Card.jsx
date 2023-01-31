b6d0e5011cc6602a35192bb79cb731e53895753d
nabilarishajuliana <99938022+nabilarishajuliana@users.noreply.github.com>
2023-01-31 21:02:32 +0700

    Update Card.jsx

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -5,24 +5,14 @@ import './css/Card.css';
 import { useState, useEffect } from "react";
 import ImageCard from "./ImageCard";
 import Footer from './Footer';
-// import data from "../data.json";
+import Data from "../data.json";
 
 function Card() {
-    const [data, setData] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [search, setSearch] = useState('');
+    // const [data, setData] = useState([]);
+    // const [isLoading, setIsLoading] = useState(true);
+    const [searchTerm, setSearchTerm] = useState("");
 
-    useEffect(() => {
-        fetch(
-            "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/1f2898db-ceb4-4565-a1eb-2d3beb8a509c/product.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230130%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230130T072405Z&X-Amz-Expires=86400&X-Amz-Signature=b33123e43a01a610a35f04c061929b26cc1e29d99ab38455a726445e348878ed&X-Amz-SignedHeaders=host&x-id=GetObject"
-        )
-            .then((res) => res.json())
-            .then((data) => {
-                setData(data);
-                setIsLoading(false);
-            })
-            .catch((err) => console.log(err));
-    }, [data]);
+
 
     return (
         <>
@@ -45,36 +35,30 @@ function Card() {
                         <input
                             style={{ color: "black" }}
                             class="seacrh"
-                            onChange={(e) => setSearch(e.target.value)}
+                            onChange={(e) => setSearchTerm(e.target.value)}
                             type="text"
                             placeholder="Search " />
                     </li>
                 </ul>
             </nav>
 
-            <div class="body">
-                <div class="text">
-                    <h2>Product</h2>
-                    <p>Choose Your Product</p>
-                </div>
-
-
-                {!isLoading && data.length === 0 && (
-                    <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>
-                )}
-
-                {isLoading ? (
-                    <h1 className="text-4xl text-center mx-auto mt-24 h-60">Loading...</h1>
-                ) : (
-                    <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
-                        {data.filter((image) => {
-                            return search.toLowerCase() === '' ? image : image.name.toLowerCase().includes(search)
-                        }).map((image) => (
-                            <ImageCard key={image.id} image={image} coba={image.image} />
-                        ))}
+            <div className="container mx-auto px-12">
+                <div class="body">
+                    <div className="text-center text-4xl font-bold p-5">
+                        <h3>Products</h3>
                     </div>
-                )}
+                    {
+                        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+                            {Data.filter((image) => {
+                                return searchTerm.toLowerCase() === '' ? image : image.name.toLowerCase().includes(searchTerm)
+                            }).map((image) => (
+                                <ImageCard key={image.id} image={image} coba={image.image} />
+                            ))}
+                        </div>
+                    }
+                </div>
             </div>
+
             <Footer />
 
         </>
