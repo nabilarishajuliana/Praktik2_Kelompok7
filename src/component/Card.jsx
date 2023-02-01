a60125ec44aab56500de8a8e4b36d56b32a6ee79
nabilarishajuliana <99938022+nabilarishajuliana@users.noreply.github.com>
2023-02-01 11:13:48 +0700

    Update Card.jsx

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -5,12 +5,73 @@ import './css/Card.css';
 import { useState, useEffect } from "react";
 import ImageCard from "./ImageCard";
 import Footer from './Footer';
-import Data from "../data.json";
+import data from "../data.json";
 
 function Card() {
-    // const [data, setData] = useState([]);
-    // const [isLoading, setIsLoading] = useState(true);
-    const [searchTerm, setSearchTerm] = useState("");
+    const [search, setSearch] = useState('');
+    const [isLoading] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+    const [q, setQ] = useState('');
+    const [searchParam] = useState(['name', 'description', 'category']);
+    const [labelCategory] = useState([
+        { value: 'All', label: 'All Product By Category' },
+        { value: 'Ready', label: 'Ready' },
+        { value: 'Sold Out', label: 'Sold Out' },
+        { value: 'Pre-Order', label: 'Pre-Order' },
+    ]);
+    const [filterParam, setFilterParam] = useState('All');
+    const [product, setProduct] = useState([]);
+
+    function cari(data) {
+        isLoading(true);
+        return data.filter((item) => {
+            if (data.category === filterParam) {
+                return searchParam.some((newItem) => {
+                    return item[newItem].toString().toLowerCase().indexOf(q.toLowerCase()) > -1;
+                });
+            } else if (filterParam === 'All') {
+                return searchParam.some((newItem) => {
+                    return item[newItem].toString().toLowerCase().indexOf(q.toLowerCase()) > -1;
+                });
+            }
+            isLoading(false);
+        });
+    }
+
+    useEffect(() => {
+        console.log(filterParam);
+        const searchTerm = search.toLowerCase();
+        let filteredData;
+        if (search === '' && filterParam === 'All') {
+            setProduct(data);
+        } else {
+            if (search !== '' && filterParam === 'All') {
+                filteredData = data.filter((value) => {
+                    return value.name.toLowerCase().match(new RegExp(searchTerm, 'g'));
+                });
+            } else if (search !== '' && filterParam !== 'All') {
+                filteredData = data.filter((value) => {
+                    return (
+                        value.name.toLowerCase().match(new RegExp(searchTerm, 'g')) &&
+                        value.category === filterParam
+                    );
+                });
+            } else if (search === '' && filterParam === 'All') {
+                filteredData = data.filter((value) => {
+                    return value.category === filterParam;
+                });
+            } else if (search === '' && filterParam !== 'All') {
+                filteredData = data.filter((value) => {
+                    return value.category === filterParam;
+                });
+            }
+            setProduct(filteredData);
+        }
+    }, [filterParam, search]);
+
+    useEffect(() => {
+        setProduct(data);
+    }, []);
 
 
 
@@ -35,7 +96,7 @@ function Card() {
                         <input
                             style={{ color: "black" }}
                             class="seacrh"
-                            onChange={(e) => setSearchTerm(e.target.value)}
+                            onChange={(e) => setSearch(e.target.value)}
                             type="text"
                             placeholder="Search " />
                     </li>
@@ -48,26 +109,36 @@ function Card() {
                         <h3>Products</h3>
                     </div>
 
-                    <div className='content-center'>
-                        <select className='bg-pink-color1 rounded-md text-center p-2 text-pink-color2 font-bold ' name="select" id="select">
-                            <option className="font-thin" value="">Category</option>
-                            <option className="font-thin" value="Ready">Ready</option>
-                            <option className="font-thin" value="Pre-Order">Pre-Order</option>
-                            <option className="font-thin"value="Sold Out">Sold Out</option>
+                    <div className="justify-center flex">
+                        <select
+                            onChange={(e) => {
+                                setFilterParam(e.target.value);
+                            }}
+                            className="custom-select px-4 py-3 rounded-md bg-pink-color1/70 text-pink-color2"
+                            aria-label="Filter Product By Category"
+                        >
+                            {labelCategory.map((item) => {
+                                return <option value={item.value}>{item.label}</option>;
+                            })}
                         </select>
+                        <span className="focus"></span>
                     </div>
 
-
-
-                    {
-                        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
-                            {Data.filter((image) => {
-                                return searchTerm.toLowerCase() === '' ? image : image.name.toLowerCase().includes(searchTerm)
-                            }).map((image) => (
-                                <ImageCard key={image.id} image={image} coba={image.image} />
-                            ))}
+                    <div className="text-center text-pink-color1 text-lg mb-8">
+                        <p>{product.length} Product found</p>
+                    </div>
+                    {isLoading && <h1 className="text-5xl text-center mx-auto mt-32">Loading ....</h1>}
+                    {!isLoading && data.length === 0 && (
+                        <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>
+                    )}
+                    {data.length !== 0 && (
+                        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4" onClick={() => setShowModal(true)}>
+                            {product
+                                .map((image) => (
+                                    <ImageCard key={image.id} image={image} coba={image.image} />
+                                ))}
                         </div>
-                    }
+                    )}
                 </div>
             </div>
 
