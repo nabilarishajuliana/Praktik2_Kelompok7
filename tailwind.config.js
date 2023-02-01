611c06d238c9c95c640a811a0540d19f9d22868f
nabilarishajuliana <99938022+nabilarishajuliana@users.noreply.github.com>
2023-02-01 10:19:31 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false,
   theme: {
+    
+      colors: {
+        pink: {
+          color1: '#ab6d80',
+          color2: '#ffffff',
+          color3: '#f1e31c',
+        },
+      }
+    
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
